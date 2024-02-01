e284d4486bc7a5728df657160aff8a685b1858b1
wrg <1570502795@qq.com>
2024-02-01 18:31:40 +0800

    update

--- html_to_pdf.py
+++ html_to_pdf.py
@@ -1,12 +1,15 @@
 import re
 import threading
 import time
-
+# from weasyprint import HTML
 import requests
 import pdfkit
 from bs4 import BeautifulSoup, Tag
 import os
 from PyPDF2 import PdfReader, PdfWriter
+import imgkit
+from fpdf import FPDF
+from PIL import Image
 
 
 def merge_pdfs(paths, output):
@@ -55,21 +58,47 @@ def fetch_html(url, headers):
         # 或者根据您的需求进行其他处理
 
 
-def convert_html_to_pdf(html, output_file, stop_event):
+def convert_html_to_pdf(html, output_pdf, stop_event, output_jpg=''):
+    options = {
+        'page-size': 'Letter',
+        'margin-top': '0.75in',
+        'margin-right': '0.75in',
+        'margin-bottom': '0.75in',
+        'margin-left': '0.75in',
+        'encoding': 'UTF-8',
+        'no-outline': None,
+        'disable-smart-shrinking': '',
+    }
     try:
         clean_html_content = clean_html(html)
-        pdfkit.from_string(clean_html_content, output_file)
+        if output_jpg:
+            imgkit.from_string(clean_html_content, output_jpg)
+            # 将图片转换为 PDF
+            image = Image.open(output_jpg)
+            image.save(output_pdf, save_all=True)
+            print(f"成功将图片转换为 PDF: {output_pdf}")
+        else:
+            pdfkit.from_string(clean_html_content, output_pdf, options=options)
+            print(f"成功将 HTML 转换为 PDF: {output_pdf}")
         if stop_event.is_set():
-            print(f"线程接收到停止信号，终止处理: {output_file}")
+            print(f"线程接收到停止信号，终止处理: {output_pdf}")
             return
-    except Exception as e:
+    except OSError as e:
         print(f"转换 HTML 到 PDF 时发生错误: {e}")
+        if output_jpg:
+            # 将图片转换为 PDF
+            image = Image.open(output_jpg)
+            image.save(output_pdf, save_all=True)
+            print(f"成功将图片转换为 PDF: {output_pdf}")
+        # 或者进行其他异常处理
+    except Exception as e:
+        print(f"发生其他异常: {e}")
         # 或者进行其他异常处理
 
 
-def convert_html_to_pdf_in_thread(input_html_file, output_pdf_file):
+def convert_html_to_pdf_in_thread(input_html_file, output_pdf_file, output_image):
     stop_event = threading.Event()
-    thread = threading.Thread(target=convert_html_to_pdf, args=(input_html_file, output_pdf_file, stop_event))
+    thread = threading.Thread(target=convert_html_to_pdf, args=(input_html_file, output_pdf_file, stop_event, output_image))
     thread.start()
     return thread
 
@@ -109,7 +138,8 @@ def monitor_thread(thread, stop_event, timeout=300):
 def main(ebook_id, number, headers):
     threads = []  # 用于存储线程的列表
     for num in range(1, number + 1):
-        output_file = f"{ebook_id}/{num}.pdf"
+        output_pdf = f"{ebook_id}/{num}.pdf"
+        output_jpg = f"{ebook_id}/{num}.jpg"
         url = f"https://book.qq.com/book-read/{ebook_id}/{num}"
         try:
             html = fetch_html(url, headers)
@@ -117,12 +147,12 @@ def main(ebook_id, number, headers):
             # thread = convert_html_to_pdf_in_thread(html, output_file)
             # threads.append(thread)
             stop_event = threading.Event()
-            thread = threading.Thread(target=convert_html_to_pdf, args=(html, output_file, stop_event))
+            thread = threading.Thread(target=convert_html_to_pdf, args=(html, output_pdf, stop_event, output_jpg))
             thread.start()
             monitor = threading.Thread(target=monitor_thread, args=(thread, stop_event))
             monitor.start()
             threads.append((thread, monitor))
-            print("PDF successfully created as", output_file)
+            print("PDF successfully created as", output_pdf)
         except Exception as e:
             print("An error occurred:", e)
 
@@ -142,10 +172,11 @@ def extract_number(text):
 
 def get_book_content(ebook_id, book_name):
     url = f'https://book.qq.com/book-chapter/{ebook_id}'
-    # url = f"https://book.qq.com/book-read/32436084/146"
+    # url = f"https://book.qq.com/book-read/32436084/51"
     # response = requests.get(url, headers=headers)
     # response.raise_for_status()
-    output_file = f"{ebook_id}/0.pdf"
+    output_pdf = f"{ebook_id}/0.pdf"
+    output_jpg = f"{ebook_id}/0.jpg"
     html = fetch_html(url, headers)
     # 使用 BeautifulSoup 解析 HTML
     soup = BeautifulSoup(html, 'html.parser')
@@ -159,7 +190,7 @@ def get_book_content(ebook_id, book_name):
     # else:
     #     print("未找到书名")
     number = extract_number(book_chapter_num)
-    return book_name, number, html, output_file
+    return book_name, number, html, output_pdf, output_jpg
 
 
 if __name__ == "__main__":
@@ -170,31 +201,31 @@ if __name__ == "__main__":
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
     }
     # book_list = [32436084, 32856983, 48162894, 47184389, 34916570, 47755419, 26297912, 25916013, 24027607]
-    book_list = [33211566, 622012]
-    # book_list = [36090231, 654450]
+    # book_list = [33211566, 622012]
+    book_list = [36090231, 654450]
     book_name = {}
     for ebook_id in book_list:
         number = 0
         try:
             os.mkdir(f'{ebook_id}')
-            book_name, number, html, output_file = get_book_content(ebook_id, book_name)
+            book_name, number, html, output_pdf, output_jpg = get_book_content(ebook_id, book_name)
             print(book_name)
             # print(number)
-            # with open(f"{ebook_id}/0.html", 'w', encoding='utf-8') as file:
+            # with open(f"{ebook_id}/1.html", 'w', encoding='utf-8') as file:
             #     file.write(html)
-            convert_html_to_pdf_in_thread(html, output_file)
+            convert_html_to_pdf_in_thread(html, output_pdf, output_jpg)
         except Exception:
             pass
 
         main(ebook_id, number, headers)
-
-    # clean_html('temp.html')
-
+    #
+    # # clean_html('temp.html')
+    #
     for ebook_id in book_list:
-        # book_dir = str(book)  # 假设目录名就是书籍编号
+        # book_dir = str(ebook_id)  # 假设目录名就是书籍编号
         # pdf_files = list(find_pdfs(book_name[ebook_id])
         print('开始合书')
-        print(book_name)
+        # print(book_name)
         book_dir = book_name[ebook_id]
         pdf_files = list(find_pdfs(str(ebook_id)))
         if pdf_files:
